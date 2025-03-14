49d9eef78237b234991718f819057aaf9359cc64
Warce368 <12345marce12345@gmail.com>
2025-03-13 19:08:38 -0500

    ojala centre

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-        vite: {
-        server: {
-            watch: {
-            usePolling: true,
-            },
-        },
-        },
-});
+export default defineConfig({});
